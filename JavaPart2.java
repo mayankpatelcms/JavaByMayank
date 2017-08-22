79eb912bf7aff2bd9b19319a0741a30e301404d9
Mayank Patel <mayankpatel.cms@gmail.com>
2017-08-22 21:37:08 +0530

    File Created in Part 2

---
+++ JavaPart2.java
@@ -0,0 +1,50 @@
+
+/**
+ * Tutorial Project by Mayank
+ */
+
+public class Tutorial {
+
+    public static void main(String[] args) {
+    
+        // Intiziling Variables and DOM
+        int x = 2;
+        int y = 3;
+        
+        int add, sub, mul, div;
+        
+        // Adding Value
+        add = x+y;
+        
+        // SubtractingValue
+        sub = x-y;
+        
+        // Multiplying Value
+        mul = x*y;
+        
+        // Dividing
+        div = x/y;
+    
+        // Doing Calac() Calculation
+        System.out.println(" Var X has the value  "   + x);
+        System.out.println(" Var Y has the value  " +y);
+        
+        // Addition
+        System.out.println(" Addition of the value x and y is  " +add);
+        
+        // Subtraction
+        System.out.println(" Subtraction of value of x and y " +sub);
+        
+        // Multiplication
+        System.out.println(" Multiplication  of value of x and y " +mul);
+        
+        // Division
+        System.out.println(" Division  of value of x and y " +div);
+        
+        /* Thanks for Watching */
+        
+        /* Making 3rd part soon */
+        
+    }
+    
+}
